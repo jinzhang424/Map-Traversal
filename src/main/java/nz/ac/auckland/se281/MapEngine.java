fd94649f100c415522940b1317aba94d07ab9015
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-19 15:18:12 +1200

    Added new field riskMap in class MapEngine that maps CountryNodes to their adjacent CountryNodes and used loadMap to generate it all

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,11 @@ import java.util.Map;
 public class MapEngine {
 
   private Map<String, CountryNode> nameToNodeMap;
+  private Map<CountryNode, ArrayList<CountryNode>> riskMap;
 
   public MapEngine() {
 
+    riskMap = new HashMap<>();
     nameToNodeMap = new HashMap<>();
 
     loadMap(); // keep this mehtod invocation
@@ -20,14 +23,30 @@ public class MapEngine {
   private void loadMap() {
 
     String[] countryDetails = null; // Stores the details of a country
+    String[] adjacentCountry = null; // Stores the country name and name of adjacent countries
 
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
+
+    ArrayList<CountryNode> adjacentCountryNodes = null;
 
     // Mapping country names to their corresponding nodes
     for (String country : countries) {
       countryDetails = country.split(",");
       nameToNodeMap.put(countryDetails[0], new CountryNode(countryDetails[0], countryDetails[2]));
+    }
+
+    // Mapping a node to a list of adjacent nodes
+    for (String c : adjacencies) {
+      adjacentCountry = c.split(",");
+      adjacentCountryNodes = new ArrayList<>();
+
+      for (int i = 1; i < adjacentCountry.length; i++) {
+        // Getting the adjacent country's node from nameToNodeMap and adding it to the ArrayList:
+        // adjacentCountryNodes
+        adjacentCountryNodes.add(nameToNodeMap.get(adjacentCountry[i]));
+      }
+      riskMap.put(nameToNodeMap.get(adjacentCountry[0]), adjacentCountryNodes);
     }
   }
 
