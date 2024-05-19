4640d95ae1fd5c5e1a89392c80407fe2578310f9
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-19 16:21:57 +1200

    Moved the logic for creating that deal with maps to CountryGraph instead

---
+++ src/main/java/nz/ac/auckland/se281/CountryGraph.java
@@ -0,0 +1,46 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CountryGraph {
+
+  private Map<String, CountryNode> nameToNodeMap;
+  private Map<CountryNode, ArrayList<CountryNode>> riskMap;
+
+  public CountryGraph() {
+    nameToNodeMap = new HashMap<>();
+    riskMap = new HashMap<>();
+  }
+
+  public void generateNameToNodeMap(List<String> countries) {
+
+    String[] countryDetails = null; // Stores the details of a country
+
+    for (String country : countries) {
+      countryDetails = country.split(",");
+      nameToNodeMap.put(countryDetails[0], new CountryNode(countryDetails[0], countryDetails[2]));
+    }
+  }
+
+  public void generateRiskMap(List<String> adjacencies) {
+
+    String[] adjacentCountry = null; // Stores the country name and name of adjacent countries
+    ArrayList<CountryNode> adjacentCountryNodes = null;
+
+    // Mapping a node to a list of adjacent nodes
+    for (String c : adjacencies) {
+      adjacentCountry = c.split(",");
+      adjacentCountryNodes = new ArrayList<>();
+
+      for (int i = 1; i < adjacentCountry.length; i++) {
+        // Getting the adjacent country's node from nameToNodeMap and adding it to the ArrayList:
+        // adjacentCountryNodes
+        adjacentCountryNodes.add(nameToNodeMap.get(adjacentCountry[i]));
+      }
+      riskMap.put(nameToNodeMap.get(adjacentCountry[0]), adjacentCountryNodes);
+    }
+  }
+}
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,20 +1,13 @@
 package nz.ac.auckland.se281;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
 
-  private Map<String, CountryNode> nameToNodeMap;
-  private Map<CountryNode, ArrayList<CountryNode>> riskMap;
+  private CountryGraph countryGraph = new CountryGraph();
 
   public MapEngine() {
-
-    riskMap = new HashMap<>();
-    nameToNodeMap = new HashMap<>();
 
     loadMap(); // keep this mehtod invocation
   }
@@ -22,32 +15,11 @@ public class MapEngine {
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
 
-    String[] countryDetails = null; // Stores the details of a country
-    String[] adjacentCountry = null; // Stores the country name and name of adjacent countries
-
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
 
-    ArrayList<CountryNode> adjacentCountryNodes = null;
-
-    // Mapping country names to their corresponding nodes
-    for (String country : countries) {
-      countryDetails = country.split(",");
-      nameToNodeMap.put(countryDetails[0], new CountryNode(countryDetails[0], countryDetails[2]));
-    }
-
-    // Mapping a node to a list of adjacent nodes
-    for (String c : adjacencies) {
-      adjacentCountry = c.split(",");
-      adjacentCountryNodes = new ArrayList<>();
-
-      for (int i = 1; i < adjacentCountry.length; i++) {
-        // Getting the adjacent country's node from nameToNodeMap and adding it to the ArrayList:
-        // adjacentCountryNodes
-        adjacentCountryNodes.add(nameToNodeMap.get(adjacentCountry[i]));
-      }
-      riskMap.put(nameToNodeMap.get(adjacentCountry[0]), adjacentCountryNodes);
-    }
+    countryGraph.generateNameToNodeMap(countries);
+    countryGraph.generateRiskMap(adjacencies);
   }
 
   /** this method is invoked when the user run the command info-country. */
