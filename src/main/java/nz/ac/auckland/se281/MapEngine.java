9f1f7e7ef426e10a091d5bf82c25e785f7d6d03b
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-19 14:38:48 +1200

    Added new field nameToNodeMap to map a country's name to a corresponding node and used method loadMap to generate this

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,20 +1,34 @@
 package nz.ac.auckland.se281;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
 
+  private Map<String, CountryNode> nameToNodeMap;
+
   public MapEngine() {
-    // add other code here if you want
+
+    nameToNodeMap = new HashMap<>();
+
     loadMap(); // keep this mehtod invocation
   }
 
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
+
+    String[] countryDetails = null; // Stores the details of a country
+
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    // add code here to create your data structures
+
+    // Mapping country names to their corresponding nodes
+    for (String country : countries) {
+      countryDetails = country.split(",");
+      nameToNodeMap.put(countryDetails[0], new CountryNode(countryDetails[0], countryDetails[2]));
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
